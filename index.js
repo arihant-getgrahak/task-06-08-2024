8e672886ea97f46e3c6cc83d89ac518a1cd65102
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-07 14:09:52 +0530

    add index.js for form validation

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ form.addEventListener("submit", function (event) {
   const email = document.getElementById("email").value;
   const message = document.getElementById("message").value;
 
+  if (name === "" || email === "" || message === "") {
+    alert("Please fill in all the fields.");
+    return;
+  }
+
   const alertMessage = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
 
   alert(alertMessage);
